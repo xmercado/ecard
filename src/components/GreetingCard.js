f182d9c0a85589a5ee40f3ee7670145cfc1caad1
Xavier Mercado <xmercado1576@gmail.com>
2021-12-13 12:01:49 -0800

    feat(greeting-card): pass useState

--- src/components/GreetingCard.js
+++ src/components/GreetingCard.js
@@ -1,6 +1,6 @@
 import winter from '../images/winter.jpg';
 
-export default function GreetingCard({ Greeting, Body, Closing}) {
+export default function GreetingCard({ Greeting, Body, Closing, setCanCustomize }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +16,7 @@ export default function GreetingCard({ Greeting, Body, Closing}) {
         </p>
         <button
           className="App-link"
+          onClick={() => setCanCustomize(true)}
         >
           Reshare This Card
         </button>
