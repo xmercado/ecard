a753b0b268592a667dbf1f8b37d51e1eb0b6436b
Xavier Mercado <xmercado1576@gmail.com>
2021-12-14 12:32:48 -0800

    refactor(app): move necessary state and files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 import GreetingCard from './components/GreetingCard';
 import CustomMessage from './components/CustomMessage';
 
 function App() {
-  const [Greeting, setGreeting] = useState('Dear Mom,');
-  const [Body, setBody] = useState('Happy Holidays!');
-  const [Closing, setClosing] = useState('From Your Son');
+  const [Greeting, setGreeting] = useState('Greeting');
+  const [Body, setBody] = useState('Body');
+  const [Closing, setClosing] = useState('Closing');
   const [canCustomize, setCanCustomize] = useState(false);
 
   return (
-    <>
+    <div className='App'>
       { canCustomize
           ?
             <CustomMessage
               Greeting={Greeting}
               Body={Body}
               Closing={Closing}
+              setGreeting={setGreeting}
+              setBody={setBody}
+              setClosing={setClosing}
               setCanCustomize={setCanCustomize}
             />
           :
@@ -28,7 +30,7 @@ function App() {
               setCanCustomize={setCanCustomize}
             />
       }
-    </>
+    </div>
   );
 }
 
